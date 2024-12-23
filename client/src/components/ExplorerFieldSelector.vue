141b77490890f291b12150cee3c065e4d6ee35d8
MelisaDavilaCanales <melisa.davila@unah.hn>
2024-12-22 19:39:09 -0600

    feat(client): Add style too searcher and selecField

--- client/src/components/ExplorerFieldSelector.vue
+++ client/src/components/ExplorerFieldSelector.vue
@@ -1,15 +1,72 @@
 <script setup lang="ts">
+
+import { useSearchTypeStore } from '@/stores/useSearchTypeStore';
+import { useEmailTableStore } from '@/stores/useEmailTableStore';
+import { usePersonStore } from '@/stores/usePersonStore';
+import { storeToRefs } from 'pinia';
+import { ref, computed } from 'vue';
+
+const searchTypeStore = useSearchTypeStore();
+const { setEmailSearchField } = useEmailTableStore();
+const { setPersonSortField } = usePersonStore();
+
+// Obtener el tipo de búsqueda desde el store
+const { searchType } = storeToRefs(searchTypeStore);
+
+const searchEmailOptions = [
+  { value: 'message_id', label: 'Message ID' },
+  { value: 'date', label: 'Date' },
+  { value: 'from', label: 'From' },
+  { value: 'to', label: 'To' },
+  { value: 'subject', label: 'Subject' },
+  { value: 'cc', label: 'CC' },
+  { value: 'bcc', label: 'BCC' },
+  { value: 'x_folder', label: 'Folder' },
+  { value: 'x_origin', label: 'Origin' },
+  { value: 'x_file_name', label: 'File name' },
+  { value: 'content', label: 'Content' },
+];
+
+const searchPersonaOptions = [
+  { value: 'name', label: 'Name' },
+  { value: 'email', label: 'Email' },
+];
+
+// Calcular las opciones dependiendo del tipo de búsqueda
+const options = computed(() => {
+  if (searchType.value === 'emails') {
+    return searchEmailOptions;
+  } else if (searchType.value === 'persons') {
+    return searchPersonaOptions;
+  }
+  return [];
+});
+
+const selectedOption = ref<string>('');
+
+function handleSelectChange(event: Event) {
+  const target = event.target as HTMLSelectElement;
+  const value = target.value;
+  selectedOption.value = value;
+  if (searchType.value === 'emails') {
+    setEmailSearchField(value);
+  } else if (searchType.value === 'persons') {
+    setPersonSortField(value);
+  }
+}
 </script>
 
 <template>
-  <div class=" p-1  w-full flex justify-center">
-    <div class="flex">
-      <p class="">Search for matches only in the field:</p>
-      <select class="">
-        <option value="">Select field</option>
-        <option value="">Option1</option>
-        <option value="">Option2</option>
-        <option value="">Option3</option>
+  <div class="p-1 w-full flex justify-center font-roboto">
+    <div class="flex space-x-2 items-center">
+      <p class="text-sm text-gray-800">Search for matches only in the field:</p>
+      <select
+        class="text-sm border  border-primarySoft p-1 rounded focus:outline-none focus:ring-1 focus:ring-primarySoft"
+        @change="handleSelectChange">
+        <option value="" disabled selected>Select field</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
       </select>
     </div>
   </div>
--- client/src/components/ExplorerSearcher.vue
+++ client/src/components/ExplorerSearcher.vue
@@ -17,15 +17,41 @@ watch(selectedOption, (newValue) => {
 
 <template>
   <div class="w-full flex flex-col justify-center items-center md:flex-row md:justify-around">
-    <input class="w-[90%] border-2 px-2 md:w-4/6" type="text" placeholder="Buscar" />
-    <div class="space-x-2">
-      <label for="">Search by:</label>
 
-      <input type="radio" id="emails" value="emails" v-model="selectedOption" />
-      <label for="emails">Emails</label>
+    <div
+      class="w-2/3 flex items-center max-w-3xl mx-auto bg-white border border-primarySoft rounded-lg p-1 focus:ring-1 focus:ring-primarySoft">
+      <div class="px-1">
+        <img class="w-6" src="../assets/img/search.png" alt="">
+      </div>
 
-      <input type="radio" id="persons" value="persons" v-model="selectedOption" />
-      <label for="persons">Persons</label>
+      <input type="text" placeholder="Buscar notas"
+        class="flex-1 text-sm bg-transparent text-gray-700 placeholder-gray-400 focus:outline-none" />
+
+      <button
+        class="ml-3 bg-primaryMiddle text-white text-xs py-1 px-3 rounded hover:bg-primaryDark focus:outline-none">
+        Buscar
+      </button>
     </div>
+
+
+    <div
+      class="w-1/3 flex items-center justify-center ml-3 space-x-4 bg-primaryExtraSoft px-1 py-2 rounded-lg font-light text-xs">
+      <span class="font-normal text-sm text-gray-800 leading-none ">Search by:</span>
+
+      <div class="flex items-center space-x-2">
+        <input type="radio" id="emails" value="emails" v-model="selectedOption" />
+        <label for="emails" class="font-normal text-sm text-gray-800cursor-pointer leading-none">
+          Emails
+        </label>
+      </div>
+
+      <div class="flex items-center space-x-2">
+        <input type="radio" id="persons" value="persons" v-model="selectedOption" />
+        <label for="persons" class="font-normal text-sm text-gray-800 cursor-pointer leading-none">
+          Persons
+        </label>
+      </div>
+    </div>
+
   </div>
 </template>
