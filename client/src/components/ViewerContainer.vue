7f21c72d255b8cc0be39af42069776a7a14793ad
MelisaDavilaCanales <melisa.davila@unah.hn>
2024-12-27 22:29:15 -0600

    feat(client): delete viewerNavigation

--- client/src/components/ViewerContainer.vue
+++ client/src/components/ViewerContainer.vue
@@ -1,11 +1,29 @@
 <script setup lang="ts">
-import ViewerNavigation from '@/components/ViewerNavigation.vue'
-import ViewerDisplay from '@/components/ViewerDisplay.vue'
+import { storeToRefs } from "pinia"
+
+import PersonDetail from '@/components/PersonDetail.vue'
+import EmailDetail from '@/components/EmailDetail.vue'
+
+import { useItemSelectedStore } from '@/stores/useItemSelectedStore'
+
+
+const itemSelectedStore = useItemSelectedStore()
+const { isEmailSelected, isPersonSelected, isItemSelected } = storeToRefs(itemSelectedStore)
+
 </script>
 
 <template>
   <div class="h-screen flex flex-col items-center justify-center md:h-full  py-4 px-4 space-y-2">
-    <ViewerNavigation></ViewerNavigation>
-    <ViewerDisplay></ViewerDisplay>
+    <div :class="{ 'h-[90%] overflow-hidden w-full ': isItemSelected }"
+      class="h-[90%] w-full bg-grayExtraSoft rounded-md p-4">
+      <PersonDetail v-if="isPersonSelected" />
+      <EmailDetail v-if="isEmailSelected" />
+      <div v-if="!isPersonSelected && !isEmailSelected"
+        class="h-full grayExtraSoft w-full bg-grayExtraSoft flex flex-col justify-center items-center">
+        <img src="../assets/img/kitty.png" alt="" class="w-28 md:w-40 pb-4">
+        <h4 class="font-roboto text-2xl text-grayDark ">Visualizer</h4>
+        <p class="font-roboto  text-grayDark">[Select an item to display it in detail]</p>
+      </div>
+    </div>
   </div>
 </template>
