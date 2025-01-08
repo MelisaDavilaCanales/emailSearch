95e26760b5cd4d0033f9ea4246e88167021d2c3f
Ubuntu <ubuntu@ip-172-31-92-190.ec2.internal>
2025-01-08 08:15:34 +0000

    changes in env vars

--- indexer/constant/workerpools.go
+++ indexer/constant/workerpools.go
@@ -3,17 +3,17 @@ package constant
 const (
 
 	// BUFFER_CAPACITY is the capacity of the buffer for the channels.
-	BUFFER_CAPACITY = 50
+	BUFFER_CAPACITY = 500
 
 	// PROCESS_EMAILS_WORKERS_COUNT Number of workers (goroutines) for the email-processing WorkerPool
-	PROCESS_EMAILS_WORKERS_COUNT = 2
+	PROCESS_EMAILS_WORKERS_COUNT = 1
 
 	// SEND_EMAILS_WORKERS_COUNT Number of workers (goroutines) for the email-sending WorkerPool
-	SEND_EMAILS_WORKERS_COUNT = 2
+	SEND_EMAILS_WORKERS_COUNT = 1
 
 	// STRUCTURE_PERSONS_WORKERS_COUNT Number of workers (goroutines) for the person-structuring WorkerPool
-	STRUCTURE_PERSONS_WORKERS_COUNT = 2
+	STRUCTURE_PERSONS_WORKERS_COUNT = 1
 
 	// BUILD_PERSON_BATCHES_WORKERS_COUNT Number of workers (goroutines) for the personBatches-building WorkerPool
-	BUILD_PERSON_BATCHES_WORKERS_COUNT = 2
+	BUILD_PERSON_BATCHES_WORKERS_COUNT = 1
 )
